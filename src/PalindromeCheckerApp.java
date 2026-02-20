643086e2f4f6d0fedb670ea4b89dbb54950bd7a1
azezal1 <kaushikgarv3107@gmail.com>
2026-02-20 09:59:27 +0530

    code changes

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,22 +2,6 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Hardcoded string literal
-        String word = "madam";
 
-        // Reverse the string
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
-        }
-
-        // Check if palindrome using conditional statement
-        if (word.equals(reversed)) {
-            System.out.println("The word \"" + word + "\" is a palindrome.");
-        } else {
-            System.out.println("The word \"" + word + "\" is not a palindrome.");
-        }
-
-        // Program exits
     }
 }
