b7e3de6e74ee20be63482791952a64a9124e34ef
azezal1 <kaushikgarv3107@gmail.com>
2026-02-20 10:04:39 +0530

    UC1: welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,16 @@
 public class PalindromeCheckerApp {
 
+    // Application constants
+    static final String APP_NAME = "Palindrome Checker Management";
+    static final String VERSION = "Version 1.0";
+
     public static void main(String[] args) {
 
 
+        System.out.println("Welcome to " + APP_NAME);
+        System.out.println(VERSION);
+
+        // Program continues to next use case or exits
+        System.out.println("Application is ready.");
     }
 }
