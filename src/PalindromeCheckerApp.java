dcb5c5f046f5af1fdf305c849ba904016aa94b22
sidTan-coder <siddhant.tandon06@gmail.com>
2026-02-20 09:05:10 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+public class PalindromeCheckerApp {
+
+    // Application constants
+    static final String APP_NAME = "Palindrome Checker App";
+    static final String VERSION = "Version 1.0";
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to " + APP_NAME);
+        System.out.println(VERSION);
+
+
+
+        System.out.println("Application is ready to check palindromes.");
+
+
+    }
+}
